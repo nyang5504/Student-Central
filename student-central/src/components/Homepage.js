90999d900b3f6f2eac57e54913a78fcde02412aa
Mohamed Barrie <mohamedbarrie178@gmail.com>
2023-06-25 20:16:58 -0400

    Navbar push

---
+++ student-central/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+import '../styles/Homepage.css';
+
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            <NavBar />
+        </div>
+    );
+};
+
+export default HomePage;
