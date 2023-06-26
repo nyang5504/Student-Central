cd331d0795e4dfa3878e817ffadbb118fe1c7766
Mohamed Barrie <mohamedbarrie178@gmail.com>
2023-06-25 23:15:36 -0400

    Footer Push

---
+++ student-central/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2>About Us</h2>
+      <div className="card-container">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- student-central/src/components/Homepage.js
+++ student-central/src/components/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Footer from './Footer';
 import '../styles/Homepage.css';
 
 const HomePage = () => {
     return (
         <div className="homepage-container">
             <NavBar />
+            <Footer />
         </div>
     );
 };
