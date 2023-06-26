5c9c73dfb64ad04594624bcd241449a2a5d085d4
Mohamed Barrie <mohamedbarrie178@gmail.com>
2023-06-26 16:23:04 -0400

    Quick edit of Footer

--- student-central/src/components/Footer.js
+++ student-central/src/components/Footer.js
@@ -6,10 +6,18 @@ const Footer = () => {
     <footer className="footer">
       <h2>About Us</h2>
       <div className="card-container">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card">
+            <h2>Mohamed Barrie</h2>
+        </div>
+        <div className="card">
+            <h2>Mary Tang</h2>
+        </div>
+        <div className="card">
+        <h2>Nina Yang</h2>
+        </div>
+        <div className="card">
+            <h2>Navkiran Kaur</h2>
+        </div>
       </div>
     </footer>
   );
