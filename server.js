b446c93beb8793fc72cabcbf126541549fa7f459
nyang5504 <ninayang3.14@gmail.com>
2023-08-12 20:32:38 -0400

    fix refresh

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // Parses cookies
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'student-central/build')));
 
 // MongoDB url and password
 const uri = `mongodb+srv://${username}:${password}@studentcentral.mci0sqm.mongodb.net/`;
